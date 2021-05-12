a520e9381208bf7efacffac8f227e365febf7365
Mihai Alexandru Michis <alexandru.michis@ro.softvision.global>
2021-05-12 13:49:08 +0300

    5# - Add more forecast information.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,20 @@
-const request = require('postman-request')
+const request = require('postman-request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=6770a9b456391cc46d70b054e7524cf2&query=${latitude},${longitude}`
-    
-    request ({ url, json: true }, (error, {body} = {}) => {
-        if (error) {
-            callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`)
-        }
-    })
-}
+  const url = `http://api.weatherstack.com/current?access_key=6770a9b456391cc46d70b054e7524cf2&query=${latitude},${longitude}`;
 
-module.exports = forecast
+  request({ url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. Humidity is ${body.current.humidity}% and the UV index is ${body.current.uv_index}.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
